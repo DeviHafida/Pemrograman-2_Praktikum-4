3941e706682dd8f0f3cbda1cd697d8c0e617278c
DeviHafida <144782420+DeviHafida@users.noreply.github.com>
2024-12-07 21:59:19 +0800

    Add files via upload

--- HewanPeliharaan.java
+++ HewanPeliharaan.java
@@ -1,19 +1,19 @@
-package soal2;
+package soal1;
 
 public class HewanPeliharaan {
-    private String nama;
-    private String ras;
+	    private String nama;
+	    private String ras;
 
-    // Konstruktor untuk inisialisasi nama dan ras
-    public HewanPeliharaan(String r, String n) {
-        this.nama = n;
-        this.ras = r;
-    }
+	    // Konstruktor
+	    public HewanPeliharaan(String r, String n) {
+	        this.nama = n;
+	        this.ras = r;
+	    }
 
-    // Method untuk menampilkan detail umum hewan peliharaan
-    public void display() {
-        System.out.println("Detail Hewan Peliharaan:");
-        System.out.println("Nama hewan peliharaanku adalah : " + this.nama);
-        System.out.println("Dengan ras : " + this.ras);
-    }
-}
+	    // Method untuk menampilkan detail hewan peliharaan
+	    public void display() {
+	        System.out.println("Detail Hewan Peliharaan:");
+	        System.out.println("Nama hewan peliharaanku adalah : " + this.nama);
+	        System.out.println("Dengan ras : " + this.ras);
+	    }
+	}
